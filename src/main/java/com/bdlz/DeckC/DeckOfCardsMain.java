848b684171c464b7d02725b91e6df3ab89bf2898
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-13 09:45:11 +0530

    Added: deck of cards

--- src/main/java/com/bdlz/DeckC/DeckOfCardsMain.java
+++ src/main/java/com/bdlz/DeckC/DeckOfCardsMain.java
@@ -2,6 +2,7 @@ package com.bdlz.DeckC;
 
 public class DeckOfCardsMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to the deck of cards program");
+        PlayCards cardsPlay = new PlayCards();
+        cardsPlay.uniqueCards();
     }
 }
