94c728532412def9e9cdd863712a78b9c82ebc2c
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-13 08:59:03 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/DeckC/DeckOfCardsMain.java
@@ -0,0 +1,7 @@
+package com.bdlz.DeckC;
+
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the deck of cards program");
+    }
+}
